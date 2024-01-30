33c0dcc77994b588c6682a2107d3ea1f51d61cef
Nicholas J Dunn <nicholasduunn@gmail.com>
2024-01-30 14:13:12 -0800

    added some types for bt devices and tindeq opcode/uuid information

--- App.tsx
+++ App.tsx
@@ -1,12 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import useBLE from './hooks/useBLE';
+import { useState } from 'react';
+import DeviceModal from './components/BTDeviceConnectionModal';
 
 export default function App() {
+  const { requestPermissions,
+    scanForPeripherals
+  } = useBLE();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  const scanForDevices = async () => {
+    const isPermissionsEnabled = await requestPermissions();
+    if (isPermissionsEnabled) {
+      scanForPeripherals();
+    }
+  };
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const openModal = async () => {
+    scanForDevices();
+    setIsModalVisible(true);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Connect to a BT Device</Text>
+      </View>
+      <TouchableOpacity
+        onPress={openModal}
+      >
+        <Text>{"Connect"}</Text>
+      </TouchableOpacity>
+      <DeviceModal
+        closeModal={hideModal}
+        visible={isModalVisible}
+        connectToPeripheral={()=>{}}
+        devices={[]}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
---
+++ hooks/useBLE.ts
@@ -0,0 +1,103 @@
+/* eslint-disable no-bitwise */
+
+import { useMemo, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+import { BleManager, Device } from "react-native-ble-plx";
+import * as ExpoDevice from "expo-device";
+
+type BluetoothLowEnergyApi = {
+	requestPermissions: () => Promise<boolean>;
+	scanForPeripherals: () => void;
+}
+
+const useBLE = (): BluetoothLowEnergyApi => {
+	const bleManager = useMemo(() => new BleManager(), []);
+
+	const [allDevices, setAllDevices] = useState<Device[]>([]);
+
+	const requestAndroid31Permissions = async () => {
+		const bluetoothScanPermission = await PermissionsAndroid.request(
+			PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+			{
+				title: "Bluetooth Scan Permissions",
+				message: "App requires Bluetooth Scanning",
+				buttonPositive: "OK"
+			}
+		)
+
+		const bluetoothConnectPermission = await PermissionsAndroid.request(
+			PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+			{
+				title: "Bluetooth Connect Permissions",
+				message: "App requires Bluetooth Connection",
+				buttonPositive: "OK"
+			}
+		)
+
+		const bluetoothFineLocationPermission = await PermissionsAndroid.request(
+			PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+			{
+				title: "Fine Location",
+				message: "App requires Bluetooth Fine Location",
+				buttonPositive: "OK"
+			}
+		)
+		return (
+			bluetoothScanPermission === "granted" &&
+			bluetoothConnectPermission === "granted" &&
+			bluetoothFineLocationPermission === "granted"
+		)
+	}
+
+	const requestPermissions = async () => {
+		if (Platform.OS === "android") {
+			if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+				const granted = await PermissionsAndroid.request(
+					PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+					{
+						title: "Location Permission",
+						message: "App requires location permission",
+						buttonPositive: "OK"
+					}
+				);
+
+				return granted === PermissionsAndroid.RESULTS.GRANTED;
+			} else {
+				const isAndroid31PermissionGranted =
+					await requestAndroid31Permissions();
+				return isAndroid31PermissionGranted;
+			}
+		} else {
+			return true;
+		};
+
+	};
+
+	const isDuplicateDevice = (devices: Device[], nextDevice: Device) =>
+		devices.findIndex((device) => nextDevice.id === device.id) > -1;
+
+	const scanForPeripherals = () => {
+		bleManager.startDeviceScan(null, null, (error, device) => {
+			if (error) {
+				console.log(error);
+			}
+			if (device && device.name?.includes("tindeq")) {
+				setAllDevices(prevState => {
+					if (!isDuplicateDevice(prevState, device)) {
+						return [...prevState, device]
+					}
+					return prevState
+				})
+			}
+		});
+	};
+
+	return {
+		scanForPeripherals,
+		requestPermissions
+	}
+
+
+}
+
+export default useBLE;

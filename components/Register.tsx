723cf722953cfd967545db41c03daeb7e987fa25
Nicholas J Dunn <nicholasduunn@gmail.com>
2024-02-11 20:49:15 -0800

    feat(google signin): added google sign in to register component

---
+++ components/Register.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Button} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+async function onGoogleButtonPress() {
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+}
+
+function GoogleSignIn() {
+    return (
+        <Button
+            title="Google Sign-In"
+            onPress={() =>
+                onGoogleButtonPress().then(() =>
+                    console.log('Signed in with google'),
+                )
+            }
+        />
+    );
+}

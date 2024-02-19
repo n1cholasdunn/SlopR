b07d8134be3a23f09608e052283262df65c34711
Nicholas J Dunn <nicholasduunn@gmail.com>
2024-02-19 09:14:52 -0800

    feat(appstate and online manager hooks/app layout): added react query provider and hooks
    added provider for react query and hooks to manager state refreshing for react native

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,59 @@
 import {Tabs} from 'expo-router';
+import {AppStateStatus, Platform} from 'react-native';
+import {
+    QueryClient,
+    QueryClientProvider,
+    focusManager,
+} from '@tanstack/react-query';
+import {useAppState} from '../hooks/useAppState';
+import {useOnlineManager} from '../hooks/useOnlineManager';
+
+function onAppStateChange(status: AppStateStatus) {
+    if (Platform.OS !== 'web') {
+        focusManager.setFocused(status === 'active');
+    }
+}
+
+const queryClient = new QueryClient({
+    defaultOptions: {queries: {retry: 2}},
+});
 
 export default function Layout() {
+    useOnlineManager();
+    useAppState(onAppStateChange);
+
     return (
-        <Tabs>
-            <Tabs.Screen
-                name="Home"
-                options={{
-                    href: '/',
-                    title: 'Home',
-                }}
-            />
-            <Tabs.Screen
-                name="Settings"
-                options={{
-                    href: '/settings',
-                    title: 'Settings',
-                }}
-            />
-            <Tabs.Screen
-                name="Graph1"
-                options={{
-                    href: '/graph1',
-                    title: 'Graph1',
-                }}
-            />
-            <Tabs.Screen
-                name="LiveGraph"
-                options={{
-                    href: '/graph1/livegraph',
-                    title: 'Live Graph',
-                }}
-            />
-        </Tabs>
+        <QueryClientProvider client={queryClient}>
+            <Tabs>
+                <Tabs.Screen
+                    name="Home"
+                    options={{
+                        href: '/',
+                        title: 'Home',
+                    }}
+                />
+                <Tabs.Screen
+                    name="Settings"
+                    options={{
+                        href: '/settings',
+                        title: 'Settings',
+                    }}
+                />
+                <Tabs.Screen
+                    name="Graph1"
+                    options={{
+                        href: '/graph1',
+                        title: 'Graph1',
+                    }}
+                />
+                <Tabs.Screen
+                    name="LiveGraph"
+                    options={{
+                        href: '/graph1/livegraph',
+                        title: 'Live Graph',
+                    }}
+                />
+            </Tabs>
+        </QueryClientProvider>
     );
 }

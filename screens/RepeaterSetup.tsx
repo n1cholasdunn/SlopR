ec500024f1c7c23c5be4330a66354a864f1c90cd
Nicholas J Dunn <nicholasduunn@gmail.com>
2024-03-06 21:41:45 -0800

    chore(repeater setup screen): added todos for creating the repeater page

---
+++ screens/RepeaterSetup.tsx
@@ -0,0 +1,9 @@
+const RepeatersSetup = () => {
+    //TODO: add pause picker
+    //TODO: add set picker
+    //TODO: add sets picker
+    //TODO: countdown picker
+    //TODO: Start session button
+};
+
+export default RepeatersSetup;

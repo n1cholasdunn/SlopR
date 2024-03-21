0ab0842792ecde7561acdb66d8231f29140210f9
Nicholas J Dunn <nicholasduunn@gmail.com>
2024-03-21 08:34:51 -0700

    style(repeater/workout): missed save on file in last commit

--- app/repeater/workout.tsx
+++ app/repeater/workout.tsx
@@ -1,12 +1,5 @@
-import {Canvas, Path} from '@shopify/react-native-skia';
-import {useEffect, useState} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
-
 import ForceGauge from '../../components/ForceGauge';
 import LiveGraph from '../../components/LiveGraph';
-import useBLEStore from '../../stores/useBLEStore';
-import {GraphData} from '../../types/chartData';
-import {GRAPH_HEIGHT, GRAPH_WIDTH, makeGraph} from '../../utils/graph';
 
 const Page = () => {
     return <ForceGauge graphComponent={LiveGraph} />;
